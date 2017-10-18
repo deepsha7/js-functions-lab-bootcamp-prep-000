d631a1af53882f37709d0a2ad63bcabcbf32a921
Deepak Arora <deepsha898@gmail.com>
2017-10-18 22:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
 }
-happyHolidayTo("Independence Day", "you");
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
